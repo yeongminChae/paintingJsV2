2792cba2b5a5db8a134f5909cdbd7ba60cd2a3d9
Yeongmin_Chae <codudals98@naver.com>
2022-10-27 19:55:14 +0900

    Line Width

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
 canvas.width = 600;
 canvas.height = 600;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 const onMove = (event) => {
@@ -11,6 +13,7 @@ const onMove = (event) => {
     ctx.stroke();
     return;
   }
+  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY);
 };
 
@@ -22,7 +25,13 @@ const cancelPainting = () => {
   isPainting = false;
 };
 
+const onLineWidthChange = (event) => {
+  ctx.lineWidth = event.target.value;
+};
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
