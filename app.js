a27c709da9526425055404c01f83b27a89697a05
Yeongmin_Chae <codudals98@naver.com>
2022-10-27 19:03:47 +0900

    Drawing Project 2

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(210 - 38, 200 - 40, 15, 100);
+ctx.fillRect(350 - 38, 200 - 40, 15, 100);
+ctx.fillRect(260 - 38, 200 - 40, 60, 200);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(270, 80, 8, Math.PI, 2 * Math.PI);
+ctx.arc(230, 80, 8, Math.PI, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(250, 100, 20, 0, 1 * Math.PI);
 ctx.fill();
