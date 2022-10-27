cee87f9643a06a8f6a37ffeb0551b33a484a7d4b
Yeongmin_Chae <codudals98@naver.com>
2022-10-28 01:01:47 +0900

    Adding Images

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
@@ -79,6 +80,27 @@ const onEraserBtn = () => {
   modeBtn.innerText = "Fill";
 };
 
+const onFileChange = (event) => {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
+};
+// function onFileChange(event) {
+//   const file = event.target.files[0];
+//   const url = URL.createObjectURL(file);
+//   const image = new Image();
+//   image.src = url;
+//   image.onload = function () {
+//     ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+//     fileInput.value = null;
+//   };
+// }
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -92,3 +114,5 @@ colorOptions.forEach((color) => color.addEventListener("click", onColorclick));
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyBtn);
 eraserBtn.addEventListener("click", onEraserBtn);
+
+fileInput.addEventListener("change", onFileChange);
