878ae7fa489ae2f231e1464c0c8b430a5aeb1cb1
Yeongmin_Chae <codudals98@naver.com>
2022-10-27 19:48:18 +0900

    Mouse Painting

--- app.js
+++ app.js
@@ -2,42 +2,27 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 600;
-
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-  "#18dcff",
-  "#7d5fff",
-];
-
 ctx.lineWidth = 2;
-let x_coordinate = 0;
-let y_coordinate = 0;
+let isPainting = false;
 
-const onMouseClick = (event) => {
-  ctx.beginPath();
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.moveTo(x_coordinate, y_coordinate);
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+const onMove = (event) => {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 };
 
-const onMoveClick = (event) => {
-  ctx.beginPath();
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  x_coordinate = event.offsetX;
-  y_coordinate = event.offsetY;
-  ctx.moveTo(x_coordinate, y_coordinate);
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+const startPainting = () => {
+  isPainting = true;
 };
 
-canvas.addEventListener("mousemove", onMouseClick);
-canvas.addEventListener("click", onMoveClick);
+const cancelPainting = () => {
+  isPainting = false;
+};
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
