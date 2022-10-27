8e7106b3efd83cb3270e746756a0bba1d4db07cc
Yeongmin_Chae <codudals98@naver.com>
2022-10-27 18:27:16 +0900

    Paths

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200);
+// ctx.fill filling with color
+// ctx.stroke only lines
+
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+ctx.beginPath(); // disconnecting from previous setUps
+ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
+ctx.fillStyle = "red";
+ctx.fill();
