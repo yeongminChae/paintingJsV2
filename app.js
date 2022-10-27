e1105744ce2be3510973aa57e0d5567ba3e426d4
Yeongmin_Chae <codudals98@naver.com>
2022-10-28 01:12:33 +0900

    Adding Text

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const textInput = document.getElementById("text");
 const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
@@ -16,6 +17,7 @@ const CANVAS_HEIGHT = 600;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
@@ -90,17 +92,20 @@ const onFileChange = (event) => {
     fileInput.value = null;
   };
 };
-// function onFileChange(event) {
-//   const file = event.target.files[0];
-//   const url = URL.createObjectURL(file);
-//   const image = new Image();
-//   image.src = url;
-//   image.onload = function () {
-//     ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-//     fileInput.value = null;
-//   };
-// }
 
+const onDoubleClick = (event) => {
+  const text = textInput.value;
+  if (text !== "") {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "48px serif";
+    //   ctx.strokeText(text, event.offsetX, event.offsetY);
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+};
+
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
