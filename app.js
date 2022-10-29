ed45d1dfdd457e090c1fe56c580e760d9146da6b
Yeongmin_Chae <codudals98@naver.com>
2022-10-29 19:18:03 +0900

    code challenge

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const textInput = document.getElementById("text");
 const saveBtn = document.getElementById("save-btn");
 const chooseBtn = document.getElementById("choose-btn");
 const fileInput = document.getElementById("file");
+const modeFillBtn = document.getElementById("mode-fill-btn");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
 const lineWidth = document.getElementById("line-width");
+const pencilWidth = document.getElementById("pencil-width");
 const color = document.getElementById("color");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
@@ -20,9 +22,23 @@ canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 ctx.lineCap = "round";
+let isErase = false;
 let isPainting = false;
 let isFilling = false;
 let isChoosing = false;
+let isColorFilling = false;
+let isPencilResize = false;
+let PencilSize = 50;
+
+const onPencilWidthChange = (event) => {
+  if (isPencilResize === false) {
+    if (event.target.value <= 5) {
+      PencilSize = 50 - (5 - event.target.value) * 10;
+    } else if (event.target.value >= 6) {
+      PencilSize = 50 + (event.target.value - 5) * 10;
+    }
+  }
+};
 
 const onMove = (event) => {
   if (isPainting) {
@@ -40,6 +56,10 @@ const startPainting = () => {
 
 const cancelPainting = () => {
   isPainting = false;
+  if (isColorFilling === true) {
+    ctx.fill();
+  }
+  ctx.beginPath();
 };
 
 const onLineWidthChange = (event) => {
@@ -56,6 +76,16 @@ const onColorclick = (event) => {
   ctx.fillStyle = colorValue;
   ctx.strokeStyle = colorValue;
   color.value = colorValue;
+};
+
+const onModeFillClick = () => {
+  if (isColorFilling) {
+    isColorFilling = false;
+    modeFillBtn.innerText = "⚫️ Put Color";
+  } else {
+    isColorFilling = true;
+    modeFillBtn.innerText = "⚪️ Go Back?";
+  }
 };
 
 const onModeClick = () => {
@@ -79,10 +109,16 @@ const onDestroyBtn = () => {
   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 };
 
-const onEraserBtn = () => {
-  ctx.strokeStyle = "white";
-  isFilling = false;
-  modeBtn.innerText = "🩸 Fill";
+const onEraserClickBtn = () => {
+  if (isFilling === false && isColorFilling === false) {
+    ctx.strokeStyle = "black";
+  } else {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    isColorFilling = false;
+    modeBtn.innerText = "🩸 Fill";
+    modeFillBtn.innerText = "⚫️ Put Color";
+  }
 };
 
 const onFileChange = (event) => {
@@ -110,8 +146,8 @@ const onDoubleClick = (event) => {
   const text = textInput.value;
   if (text !== "") {
     ctx.save();
-    ctx.lineWidth = 1;
-    ctx.font = "48px serif";
+    ctx.lineWidth = 5;
+    ctx.font = `${PencilSize}px serif`;
     if (isChoosing === true) {
       ctx.strokeText(text, event.offsetX, event.offsetY);
     } else if (isChoosing !== true) {
@@ -137,12 +173,14 @@ canvas.addEventListener("mouseleave", cancelPainting);
 canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+pencilWidth.addEventListener("change", onPencilWidthChange);
 color.addEventListener("change", onColorChange);
 colorOptions.forEach((color) => color.addEventListener("click", onColorclick));
 
 modeBtn.addEventListener("click", onModeClick);
+modeFillBtn.addEventListener("click", onModeFillClick);
 destroyBtn.addEventListener("click", onDestroyBtn);
-eraserBtn.addEventListener("click", onEraserBtn);
+eraserBtn.addEventListener("click", onEraserClickBtn);
 saveBtn.addEventListener("click", onSaveClick);
 chooseBtn.addEventListener("click", onChooseModeClick);
 
